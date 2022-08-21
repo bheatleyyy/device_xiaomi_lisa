00bcab334e058b1bd5741258e01b9d9ac6411feb
bheatleyyy <sugakesshaa@gmail.com>
2022-08-21 23:42:11 +0800

    lisa: PixelOS momento

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lisa.mk
+    $(LOCAL_DIR)/aosp_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lisa-user \
-    arrow_lisa-userdebug \
-    arrow_lisa-eng
+    aosp_lisa-user \
+    aosp_lisa-userdebug \
+    aosp_lisa-eng
