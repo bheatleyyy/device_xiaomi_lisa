fbc4d61c3da1ed4418b665dd5eb15739683bdd60
MikdadAli Momin <mikdad.momin121@gmail.com>
2022-09-29 18:14:33 +0530

    Hello Superior

--- superior_lisa.mk
+++ superior_lisa.mk
@@ -11,14 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common PixelOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common stuff
+$(call inherit-product, vendor/superior/config/common.mk)
+
+# Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Device Stuff
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lisa
+PRODUCT_NAME := superior_lisa
 PRODUCT_MODEL := Xiaomi 11 Lite NE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
